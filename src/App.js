7a9a3cc6e1c5582897b895764d39676cc1c5b7f7
Shishir <kumarshishir256@gmail.com>
2020-03-07 22:37:53 +0530

    Renamed HomePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Homepage from "./component/common/homepage";
+import Home from "./component/common/home";
 
 function App() {
   return (
     <div className="App">
-      <Homepage></Homepage>
+      <Home></Home>
     </div>
   );
 }

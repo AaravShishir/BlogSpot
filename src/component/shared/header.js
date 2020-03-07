740de05a9264f1d963eb22dab154500c6a2d4c00
Shishir <kumarshishir256@gmail.com>
2020-03-07 22:36:24 +0530

    Header Final Change

--- src/component/shared/header.js
+++ src/component/shared/header.js
@@ -15,7 +15,24 @@ const Header = () => {
             className="d-inline-block align-top"
           />{" "}
         </Navbar.Brand>
-        <Nav className="ml-auto">
+        <Nav className="ml-auto mr-auto">
+          <Nav.Link className="txt-navlinks" href="#home">
+            Home
+          </Nav.Link>
+          <Nav.Link className="txt-navlinks" href="#about">
+            About
+          </Nav.Link>
+          <Nav.Link className="txt-navlinks" href="#portfolio">
+            Portfolio
+          </Nav.Link>
+          <Nav.Link className="txt-navlinks" href="#blogs">
+            Blogs
+          </Nav.Link>
+          <Nav.Link className="txt-navlinks" href="#contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+        <Nav>
           <Nav.Link className=" lbl-header" href="#home">
             Shishir
           </Nav.Link>

6ad76a8ad6153690a7520de5d8ae0a4058d3e4b5
Shishir <kumarshishir256@gmail.com>
2020-03-14 16:08:11 +0530

    Footer Styling Changes

--- src/component/shared/footer.js
+++ src/component/shared/footer.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "../../assets/styles/shared/footer.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import $ from "jquery";
 
 const Footer = () => {
   return (
     <>
-      <div>Test</div>
+      <div className="footer navbar-light bg-white ">
+        <div className="copyright-section ">
+          <p>Copyright @ Shishir 2020</p>
+        </div>
+        <div className="icon-section">
+          <i className="fab fa-linkedin icon-border"></i>
+          <i className="fab fa-twitter icon-border"></i>
+          <i className="fab fa-github icon-border"></i>
+        </div>
+      </div>
     </>
   );
 };

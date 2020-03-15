7779118753d1c6c8a4ee8c4f7ffc59d681462fba
Shishir <kumarshishir256@gmail.com>
2020-03-15 13:22:57 +0530

    Dashboard UI Changes

--- src/component/tabs/dashboard.js
+++ src/component/tabs/dashboard.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import $ from "jquery";
+import constant from "../../helpers/constants";
 import "../../assets/styles/tabs/dashboard.css";
 
 const Dashboard = () => {
+  const UpdateContent = () => {
+    let interval = 2000;
+    let i = 0;
+    constant.DESCRIPTION.forEach(element => {
+      setTimeout(() => {
+        PrintText(element);
+      }, interval * i);
+      i++;
+    });
+  };
+
+  const PrintText = value => {
+    $("#intro-content").text(value);
+  };
+
+  useEffect(() => {
+    UpdateContent();
+  }, []);
+
   return (
     <>
-      <div>
-        <img
-          alt=""
-          src="./images/Background.jpg"
-          className="col-sm-12 img-padding"
-        />
+      <div className="home-background">
+        <div className="home-content">
+          <div className="home-intro">
+            <p className="pull-left">I'M </p>
+            <p className="pull-right" id="intro-content"></p>
+          </div>
+          <div className="home-description">
+            <p className=""> {constant.ONEWORDDESC}</p>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/helpers/constants.js
@@ -0,0 +1,4 @@
+export default {
+  ONEWORDDESC: "Designer, Developer, Freelancer, Blogger.",
+  DESCRIPTION: ["DESIGNER", "DEVELOPER", "FREELANCER", "BLOGGER", "SHISHIR"]
+};
